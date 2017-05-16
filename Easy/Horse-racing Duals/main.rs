3b137810edbb430e2c3b400c9278c6cef031b090
Bredgren <brandon.edgren@gmail.com>
2017-05-15 22:05:58 -0700

    Add other solutions for Rust.

--- Easy/Horse-racing Duals/main.rs
+++ Easy/Horse-racing Duals/main.rs
@@ -40,3 +40,41 @@ fn main() {
     // Write an action using println!("message...");
     // To debug: print_err!("Debug message...");
 }
+
+
+// Cool solutions by someone else
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    let stdin = io::stdin();
+
+    let mut lines = stdin.lock().lines().map(|x| x.unwrap());
+
+    let n: usize = lines.next().unwrap().parse().unwrap();
+
+    let mut strengths: Vec<i32> = lines.map(|x| x.parse().unwrap())
+                                       .collect::<Vec<i32>>();
+
+    strengths.sort();
+
+    let mindiff: i32 = strengths.iter()
+                                .zip(strengths.iter().skip(1))
+                                .map(|(x, y)| (x - y).abs())
+                                .min()
+                                .unwrap();
+
+    println!("{}", mindiff);
+}
+
+//
+use std::io::*;
+
+fn main() {
+    let stdin = stdin();
+    let mut lines = stdin.lock().lines().map(Result::unwrap);
+    let _ = lines.next();
+    let mut horses: Vec<i32> = lines.map(|x| x.parse().unwrap()).collect::<Vec<i32>>();
+    horses.sort();
+    println!("{}", horses.iter().zip(horses.iter().skip(1)).map(|(x, y)| (x - y).abs()).min().unwrap());
+}
